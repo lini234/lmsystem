9107d925a2cb29157c46fb12cdd621ab09c0406d
Inioluwa Adedokun <oluwaini06@yahoo.com>
2024-06-02 00:36:49 +0100

    Stage two

--- lms/models.py
+++ lms/models.py
@@ -1,4 +1,4 @@
-from . import db
+from lms import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
@@ -9,3 +9,20 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     department = db.Column(db.String(150), nullable=False)
     is_instructor = db.Column(db.Boolean, default=False)
+    enrollments = db.relationship('Enrollment', backref='student', lazy=True)
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    code = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    instructor_name = db.Column(db.Integer, db.ForeignKey('user.otherNames'), nullable=False)
+    enrollments = db.relationship('Enrollment', backref='course', lazy=True)
+
+class Enrollment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+
+
+
+
--- lms/views.py
+++ lms/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, abort
+from flask import Blueprint, render_template, request, abort, flash, redirect, url_for
 from flask_login import login_required, current_user
 from . import db
+from .models import Course, Enrollment
 from datetime import datetime
 
 views = Blueprint('views', __name__)
@@ -11,6 +12,58 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     if current_user.is_instructor:
-        return render_template('instructor_home.html')
+        courses = Course.query.filter_by(instructor_name=current_user.otherNames)
+        return render_template('instructor_home.html', courses=courses)
     else:
         return render_template('student_home.html')
+
+@views.route('/view-courses')
+def view_courses():
+    enrolled_courses = Enrollment.query.filter_by(user_id=current_user.id).all()
+    return render_template('view_courses.html', enrolled_courses=enrolled_courses)
+
+@views.route('/create-course', methods=['GET', 'POST'])
+def create_courses():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        code = request.form.get('code')
+        description = request.form.get('description')
+        instructor_name = current_user.otherNames
+        course = Course.query.filter_by(title=title).first()
+        if course:
+            flash('Course already exist', category='error')
+        else:
+            # add user to database
+            new_course = Course(title=title, code=code, description=description, instructor_name=instructor_name)
+            db.session.add(new_course)
+            db.session.commit()
+
+            flash('Course created', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template('create_course.html')
+
+@views.route('/manage-course/<id>', methods=['GET', 'POST'])
+def manage_courses(id):
+    course = Course.query.filter_by(id=id).first()
+    return render_template('manage_course.html', course=course)
+
+@views.route('/enroll-course', methods=['GET', 'POST'])
+def enroll_course():
+    courses = Course.query.all()
+    if request.method == 'POST':
+        course_id = request.form.get('course_id')
+        print(f"Selected Course ID: {course_id}")
+
+        if course_id:
+            course = Course.query.get(course_id)
+            if course:
+                new_enrollment = Enrollment(user_id=current_user.id, course_id=course_id)
+                db.session.add(new_enrollment)
+                db.session.commit()
+                flash('You have successfully enrolled in the course!', 'success')
+            else:
+                flash('Course not found.', 'danger')
+        else:
+            flash('Invalid course selection.', 'danger')
+    return render_template('enroll_course.html', courses=courses)
